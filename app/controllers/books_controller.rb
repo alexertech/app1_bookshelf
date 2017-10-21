e23cc675340af37a530445928f3ac046ee004385
Alex B <>
2017-10-21 00:07:55 -0300

    save new book action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,4 +7,20 @@ class BooksController < ApplicationController
         @book = Book.new
     end
 
+    def save
+        @book = Book.new(book_params)
+
+        if @book.save 
+            redirect_to books_path, notice: "book saved"
+        else
+            redirect_to new
+        end
+
+    end
+
+    private
+
+    def book_params
+        params.require(:book).permit(:name,:author)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/books'     , to: 'books#index' , as: :books
-  get '/books/new' , to: 'books#new'   , as: :books_new
-
+  get  '/books'     , to: 'books#index' , as: :books
+  get  '/books/new' , to: 'books#new'   , as: :books_new
+  post '/books'     , to: 'books#save'
 end
