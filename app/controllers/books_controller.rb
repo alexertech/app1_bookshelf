423ae7a95d0cd9705cde05dd40045ee853ea2601
Alex B <>
2017-10-20 23:52:19 -0300

    new book action, view and form

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,10 @@
 class BooksController < ApplicationController
+
     def index
     end
+
+    def new
+        @book = Book.new
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/books', to: 'books#index'
+  get '/books'     , to: 'books#index' , as: :books
+  get '/books/new' , to: 'books#new'   , as: :books_new
+
 end
